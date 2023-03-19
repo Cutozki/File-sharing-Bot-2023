c75a1f356fcc4a2a1532986084e9037f2e124127
Cutozki <98546755+Cutozki@users.noreply.github.com>
2023-03-19 18:17:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,10 +19,10 @@ APP_ID = int(os.environ.get("APP_ID", "1435947"))
 API_HASH = os.environ.get("API_HASH", "b2f04a4b0b1113ee59a54eaafaf453ca")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001189663308"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1043531483"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
@@ -40,7 +40,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
+    for x in (os.environ.get("ADMINS", "1925131814").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
